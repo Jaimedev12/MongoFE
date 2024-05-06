d1a8833cc8d0ffe2d0d903fad3b933867b8c978b
JaimedevSwirvle <jaime@swirvle.com>
2024-05-06 12:50:58 -0600

    Ahí más o menillos jala

--- src/App.jsx
+++ src/App.jsx
@@ -4,107 +4,111 @@ import Notification from "./components/Notificactions";
 import InputArea from "./InputArea";
 import ListArea from "./ListArea";
 import loginService from "./services/login";
-
-
-
-
-//Listado temporal de productos
-const productos = [
-  {
-    id: 1,
-    producto: "Microprocesador i3",
-    precio: 120,
-  },
-];
+import productService from "./services/productos";
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [user, setUser] = useState(null);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState(null);
+    const [user, setUser] = useState(null);
+    const [productos, setProductos] = useState([]);
 
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedAppUser')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      
-    }
-  }, [])
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem("loggedAppUser");
+        if (loggedUserJSON) {
+            const user = JSON.parse(loggedUserJSON);
+            console.log("User: ", user);
+            setUser(user);
+        }
 
-  const handleLogin = async (event) => {
-    event.preventDefault();
+        const fetchData = async () => {
+            const data = await productService.getAllProducts();
+            const productsArray = data.map((product) => {
+                return {
+                    id: product.id,
+                    producto: product.content.producto,
+                    precio: product.content.precio,
+                };
+            });
+            setProductos(productsArray);
+        };
 
-    try {
-      const user = await loginService.login({
-        username,
-        password,
-      });
+        fetchData();
+    }, []);
 
-      window.localStorage.setItem("loggeAppUser", JSON.stringify(user));
+    const handleLogin = async (event) => {
+        event.preventDefault();
 
-      setUser(user);
-      setUsername("");
-      setPassword("");
-    } catch (exception) {
-      setErrorMessage("Wrong credentials");
-      setUsername("");
-      setPassword("");
-      setTimeout(() => {
-        setErrorMessage(null);
-      }, 3000);
-    }
-  };
+        try {
+            const user = await loginService.login({
+                username,
+                password,
+            });
 
-  const productsForm = () => (
-    <div className="container mt-5">
-      <ListArea productos={productos} />
-      <InputArea productos={productos} />
-    </div>
-  );
+            window.localStorage.setItem("loggedAppUser", JSON.stringify(user));
 
-  const loginForm = () => (
-    <div className="container">
-      <Notification mensaje={errorMessage} />
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
+            setUser(user);
+            setUsername("");
+            setPassword("");
+        } catch (exception) {
+            setErrorMessage("Wrong credentials");
+            setUsername("");
+            setPassword("");
+            setTimeout(() => {
+                setErrorMessage(null);
+            }, 3000);
+        }
+    };
+
+    const productsForm = () => (
+        <div className="container mt-5">
+            <ListArea productos={productos} />
+            <InputArea productos={productos} />
         </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
-    </div>
-  );
+    );
 
-  return (
-    <>
-      {/*{user === null && loginForm()}
+    const loginForm = () => (
+        <div className="container">
+            <Notification mensaje={errorMessage} />
+            <form onSubmit={handleLogin}>
+                <div>
+                    username
+                    <input
+                        type="text"
+                        value={username}
+                        name="Username"
+                        onChange={({ target }) => setUsername(target.value)}
+                    />
+                </div>
+                <div>
+                    password
+                    <input
+                        type="password"
+                        value={password}
+                        name="Password"
+                        onChange={({ target }) => setPassword(target.value)}
+                    />
+                </div>
+                <button type="submit">login</button>
+            </form>
+        </div>
+    );
+
+    return (
+        <>
+            {/*{user === null && loginForm()}
       {user !== null && productsForm()} */}
 
-      {user === null ? (
-        loginForm()
-      ) : (
-        <div>
-          <p>{user.name} logged-in</p>
-          {productsForm()}
-        </div>
-      )}
-    </>
-  );
+            {user === null ? (
+                loginForm()
+            ) : (
+                <div>
+                    <p>{user.name} logged-in</p>
+                    {productsForm()}
+                </div>
+            )}
+        </>
+    );
 }
 
 export default App;
--- src/InputArea.jsx
+++ src/InputArea.jsx
@@ -1,59 +1,78 @@
 import { useState } from "react";
+import productService from "./services/productos";
 
 const InputArea = (props) => {
-  const [items, setItems] = useState(props.productos);
-  const [newItem, setNewItem] = useState("");
+    const [items, setItems] = useState(props.productos);
+    const [newItem, setNewItem] = useState("");
 
-  const addItem = (event) => {
-    event.preventDefault();
-    const itemObject = {
-      id: items.length + 1,
-      producto: newItem,
-      precio: 120,
+    const addItem = (event) => {
+        console.log(items);
+        console.log(newItem);
+
+        event.preventDefault();
+        const itemObject = {
+            id: items.length + 1,
+            producto: newItem,
+            precio: 120,
+        };
+        setItems(items.concat(itemObject));
+        setNewItem("");
     };
-    setItems(items.concat(itemObject));
-    setNewItem("");
-  };
 
-  const handleItemChange = (event) => {
-    console.log(event.target.value);
-    setNewItem(event.target.value);
-  };
+    const handleItemChange = (event) => {
+        setNewItem(event.target.value);
+    };
 
-  return (
-    <>
-      <form className="row g-4 mt-5" onSubmit={addItem}>
-        <div className="col-7">
-          <div className="input-group">
-            <input
-              value={newItem}
-              onChange={handleItemChange}
-              type="text"
-              className="form-control"
-              id="inlineFormInputGroupUsername0"
-              placeholder="...agregar nuevo producto"
-            />
-          </div>
-        </div>
-        <div className="col-2">
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control"
-              id="inlineFormInputGroupUsername1"
-              placeholder="precio"
-            />
-          </div>
-        </div>
+    return (
+        <>
+            <form className="row g-4 mt-5" onSubmit={addItem}>
+                <div className="col-7">
+                    <div className="input-group">
+                        <input
+                            value={newItem}
+                            onChange={handleItemChange}
+                            type="text"
+                            className="form-control"
+                            id="inlineFormInputGroupUsername0"
+                            placeholder="...agregar nuevo producto"
+                        />
+                    </div>
+                </div>
+                <div className="col-2">
+                    <div className="input-group">
+                        <input
+                            type="text"
+                            className="form-control"
+                            id="inlineFormInputGroupUsername1"
+                            placeholder="precio"
+                        />
+                    </div>
+                </div>
 
-        <div className="col-2">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-      </form>
-    </>
-  );
+                <div className="col-2">
+                    <button type="submit" className="btn btn-primary">
+                        Submit
+                    </button>
+                </div>
+            </form>
+
+            <button
+                onClick={() => {
+                    productService
+                        .getAllProducts()
+                        .then((response) => {
+                            console.log("----v");
+                            console.log(response);
+                        })
+                        .catch((error) => {
+                            console.log("error", error);
+                        });
+                }}
+            >
+                Debug
+            </button>
+        </>
+    );
 };
 
 export default InputArea;
